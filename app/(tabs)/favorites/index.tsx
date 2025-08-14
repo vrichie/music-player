7874d23743289bbb76bb5b8fd81f36329136e926
vrichie <dev.ritchiek@gmail.com>
2025-08-14 19:22:29 +0300

    fav search

--- app/(tabs)/favorites/index.tsx
+++ app/(tabs)/favorites/index.tsx
@@ -1,10 +1,26 @@
-import { Text, View } from "react-native"
+
+import library from '@/assets/data/library.json'
+import TracksList from "components/TracksList"
+import { screenPadding } from "constants/tokens"
+import { useNavigationSearch } from "hooks/useNavigationSearch"
+import { useMemo } from "react"
+import { ScrollView, View } from "react-native"
 import { defaultStyles } from "styles"
 
 const FavoritesScreen = () => {
+    const search = useNavigationSearch({
+        searchBarOptions: {
+            placeholder: "Find in songs"
+        }
+    })
+    const favoriteTracks = useMemo(() => {
+        return library.filter(track => track.rating === 1)
+    }, [])
     return (
         <View style={defaultStyles.container}>
-            <Text style={defaultStyles.text}> Favorites Screen</Text>
+            <ScrollView style={{ paddingHorizontal: screenPadding.horizontal }} contentInsetAdjustmentBehavior="automatic">
+                <TracksList tracks={favoriteTracks} />
+            </ScrollView>
         </View>
     )
 }
